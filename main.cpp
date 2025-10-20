dc49bb39436bea1265ba1b47bf9cedb3b4735468
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-10-20 12:48:13 +0300

    main4

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 
 int ** create (size_t rows, size_t cols,)
+{
+	
+}
+
 void remove (int ** m, size_t rows, size_t cols)
+{
+	
+}
+
+void input (int ** m, size_t rows, size_t cols) 
+{
+	
+}
+
+void output (const int * const * m, size_t rows, size_t cols)
+{
+	
+}
 
 int main()
-{size_t rows=0, cols=0;
-std::cin >> rows >> cols;
-if (!std::cin) {
-	return 1;
-}
-std::cout << rows << " " << cols;
-std::cout << "\n";
-int ** m = create (rows, cols);
+{
+	size_t rows=0, cols=0;
+	std::cin >> rows >> cols;
+	if (!std::cin) {
+		return 1;
+	}
+	int ** m = create (rows, cols);
+	input (m, rows, cols);
+	output (m, rows, cols);
+	remove (m, rows, cols);
+	std::cout << rows << " " << cols;
+	std::cout << "\n";
 }

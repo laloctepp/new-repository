c1982320564f3fd52667e2efa51f9cf9751e3688
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-10-21 01:18:44 +0300

    add convert fix

--- convert.cpp
+++ convert.cpp
@@ -30,17 +30,6 @@ int ** convert (const int * t, size_t n, const size_t * lns, size_t rows) {
     return result;
 }
 
-void output (const int * const * m, size_t rows, size_t cols) {
-	std::cout << rows << " " << cols << "\n"; 
-	for (size_t i=0; i < rows; ++i) {
-		std::cout << m[i][0];
-		for (size_t j=1; j < cols; ++j) {
-			std::cout << " " << m[i][j]; 
-		}
-		std::cout << "\n";
-	}
-}
-
 int main() {
     int t[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int n = 10;
